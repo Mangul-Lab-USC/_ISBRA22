4528572680c1e56e77773dad1950cc520eb6bf9a
Sorcery229 <63123377+Sorcery229@users.noreply.github.com>
2022-10-11 18:04:45 +0300

    link

--- pages/agenda.js
+++ pages/agenda.js
@@ -3,9 +3,18 @@ import styles from "../styles/general.module.css";
 function Agenda() {
   return (
     <>
-    <h1>ISBRA 2022 Program</h1>
+    <h1>Program agenda</h1>
+    <div className={styles.notice}>
+        <a className={styles.link} rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1KhirD4vE5u7A2cJLCvn2ZFUQ-A0pF0WC/view?usp=sharing">
+          ISBRA 2022 Program (preliminary draft)
+        </a>
+     </div>
     </>
   );
 }
 
 export default Agenda;
+
+
+import styles from "../styles/general.module.css";
+
