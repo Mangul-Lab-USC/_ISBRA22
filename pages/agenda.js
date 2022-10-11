03a6f323ab4273ea13bca657159467b8b38486b8
Sorcery229 <63123377+Sorcery229@users.noreply.github.com>
2022-10-11 15:45:48 +0300

    s

--- pages/agenda.js
+++ pages/agenda.js
@@ -4,24 +4,23 @@ function Agenda() {
   return (
     <>
     <h1>ISBRA 2022 Program</h1>
-            <table>
-            <thead>
-              <tr>
-                <th>9:00am-<br>10:00am</th>
-                <th>Plenary Session Room: TBA<br>Chair:TBA</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td></td>
-                <td>Session 1:<br>Keynote Talk<br>Talk Title<br>Eugene Koonin, National Center for Biotechnology Information,<br>National Institutes of Health</td>
-              </tr>
-              <tr>
-                <td>10:00am-<br>10:20am</td>
-                <td>Coffee break<br></td>
-              </tr>
-            </tbody>
-            </table>
+        <table>
+          <tr>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+          </tr>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+          </tr>
+          <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+          </tr>
+        </table>
     </>
   );
 }
