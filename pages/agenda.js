d9c654682e23015a2f5d4bdd6e9d6066bbcd32c6
Sorcery229 <63123377+Sorcery229@users.noreply.github.com>
2022-10-11 15:35:58 +0300

    u

--- pages/agenda.js
+++ pages/agenda.js
@@ -4,14 +4,23 @@ function Agenda() {
   return (
     <>
     <h1>ISBRA 2022 Program</h1>
-    <table>
-    <thead>
-       <tr>
-       <th>9:00am-<br></br>10:00am</th>
-       <th>Plenary Session Room: TBA<br></br>Chair:TBA</th>
-    </tr>
-    </thead>
-    </>
+        <table>
+          <tr>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+          </tr>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+          </tr>
+          <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+          </tr>
+        </table>
   );
 }
 
