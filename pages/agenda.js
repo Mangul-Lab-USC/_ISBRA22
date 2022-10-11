3673136afb4cda40d55f7f2a63df6e11d6f0c84f
Sorcery229 <63123377+Sorcery229@users.noreply.github.com>
2022-10-11 14:20:35 +0300

    r

--- pages/agenda.js
+++ pages/agenda.js
@@ -5,27 +5,7 @@ function Agenda() {
   return (
     <>
       <h1>ISBRA 2022 Program</h1>
-      <h1>(preliminary draft)</h1>
-      <br></br>
-      <h2>Tuesday, November 15th, 2022</h2>
-            <table class="tg">
-            <thead>
-              <tr>
-                <th class="tg-ljfq"><span style="font-weight:bold">9:00am-</span><br><span style="font-weight:bold">10:00am</span></th>
-                <th class="tg-ljfq"><span style="font-weight:bold">Plenary Session Room: TBA</span><br><span style="font-weight:bold">Chair:TBA</span></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td class="tg-byi7"></td>
-                <td class="tg-0pky"><span style="font-weight:bold">Session 1:</span><br><span style="font-weight:bold">Keynote Talk</span><br>Talk Title<br><span style="font-style:italic">Eugene Koonin, National Center for Biotechnology Information,</span><br><span style="font-style:italic">National Institutes of Health</span></td>
-              </tr>
-              <tr>
-                <td class="tg-ljfq"><span style="font-weight:bold">10:00am-</span><br><span style="font-weight:bold">10:20am</span></td>
-                <td class="tg-ljfq"><span style="font-weight:bold">Coffee break</span><br></td>
-              </tr>
-            </tbody>
-            </table>
+
     </>
   );
 }
