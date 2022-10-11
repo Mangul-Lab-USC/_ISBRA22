e7447dc7dbbf3e47cd2009ec4333e5b60ac86166
Sorcery229 <63123377+Sorcery229@users.noreply.github.com>
2022-10-11 15:26:14 +0300

    u

--- pages/agenda.js
+++ pages/agenda.js
@@ -7,7 +7,7 @@ function Agenda() {
 <table>
 <thead>
   <tr>
-    <th>9:00am-<br>10:00am</th>
+    <th>9:00am-<br></br>10:00am</th>
     <th>Plenary Session Room: TBA<br></br>Chair:TBA</th>
   </tr>
 </thead>
