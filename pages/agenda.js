61029ba5b3c3536087e6f0449d34db26cce583d2
Sorcery229 <63123377+Sorcery229@users.noreply.github.com>
2022-10-25 13:06:33 +0300

    f

--- pages/agenda.js
+++ pages/agenda.js
@@ -8,11 +8,37 @@ function Agenda() {
         <a className={styles.link} rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1KhirD4vE5u7A2cJLCvn2ZFUQ-A0pF0WC/view?usp=sharing">
           ISBRA 2022 Program (preliminary draft)
         </a>
-     </div>
+    <style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg .tg-byi7{background-color:#e6f3ff;border-color:inherit;text-align:left;vertical-align:top}
+.tg .tg-ljfq{background-color:#9cc2e5;border-color:inherit;text-align:left;vertical-align:top}
+.tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
+</style>
+<table class="tg">
+<thead>
+  <tr>
+    <th class="tg-ljfq"><span style="font-weight:bold">9:00am-</span><br><span style="font-weight:bold">10:00am</span></th>
+    <th class="tg-ljfq"><span style="font-weight:bold">Plenary Session Room: TBA</span><br><span style="font-weight:bold">Chair:TBA</span></th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td class="tg-byi7"></td>
+    <td class="tg-0pky"><span style="font-weight:bold">Session 1:</span><br><span style="font-weight:bold">Keynote Talk</span><br>Talk Title<br><span style="font-style:italic">Eugene Koonin, National Center for Biotechnology Information,</span><br><span style="font-style:italic">National Institutes of Health</span></td>
+  </tr>
+  <tr>
+    <td class="tg-ljfq"><span style="font-weight:bold">10:00am-</span><br><span style="font-weight:bold">10:20am</span></td>
+    <td class="tg-ljfq"><span style="font-weight:bold">Coffee break</span><br></td>
+  </tr>
+</tbody>
+</table>
+     
     </>
   );
 }
 
 export default Agenda;
-
-
