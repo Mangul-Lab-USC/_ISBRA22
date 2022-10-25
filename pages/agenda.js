c0fd567da79df04cf12ab5e0c94821303e2e3035
Sorcery229 <63123377+Sorcery229@users.noreply.github.com>
2022-10-25 13:38:13 +0300

    po

--- pages/agenda.js
+++ pages/agenda.js
@@ -7,6 +7,24 @@ function Agenda() {
     <div className={styles.notice}>
         <a className={styles.link} rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1KhirD4vE5u7A2cJLCvn2ZFUQ-A0pF0WC/view?usp=sharing">
           ISBRA 2022 Program (preliminary draft)
+    <table>
+        <thead>
+          <tr>
+            <th>9:00am-<br></br>10:00am</th>
+            <th>Plenary Session Room: TBA<br></br>Chair:TBA</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td>Session 1:<br></br>Keynote Talk<br></br>Talk Title<br></br>Eugene Koonin, National Center for Biotechnology Information,<br></br>National Institutes of Health</td>
+          </tr>
+          <tr>
+            <td>10:00am-<br></br>10:20am</td>
+            <td>Coffee break<br></br></td>
+          </tr>
+        </tbody>
+    </table>
     </>
   );
 }
