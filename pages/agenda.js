7ceb1dc1a26fc5115b47762b6dc693c054b070a9
Sorcery229 <63123377+Sorcery229@users.noreply.github.com>
2022-11-03 09:29:51 +0300

    Update agenda.js

--- pages/agenda.js
+++ pages/agenda.js
@@ -4,8 +4,8 @@ function Agenda() {
     <>
     <h1>Program agenda</h1>
     <div className={styles.notice}>
-        <a className={styles.link} rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1-CYsAITSywNtWzq4oSsji_B6u6k2oi07/view?usp=sharing">
-          ISBRA 2022 Program (preliminary draft)
+        <a className={styles.link} rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1I-NeYU5DtGtlze-L6cxhiYNzCblvrO7A/view?usp=sharing">
+          ISBRA 2022 Program
         </a>
     </div>
     </>
