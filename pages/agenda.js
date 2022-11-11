d32c63037bab17b5b3879ba7bc3bf5de3a6a2bf1
Sorcery229 <63123377+Sorcery229@users.noreply.github.com>
2022-11-11 09:50:55 +0200

    Update agenda.js

--- pages/agenda.js
+++ pages/agenda.js
@@ -17,13 +17,13 @@ function Agenda() {
           ISBRA 2022 Program
         </a>
     </div>
-    <img src="isbra_program_p1">
-    <img src="isbra_program_p2">
-    <img src="isbra_program_p3">
-    <img src="isbra_program_p4">
-    <img src="isbra_program_p5">
-    <img src="isbra_program_p6">
-    <img src="isbra_program_p7">
+    <img src=isbra_program_p1>
+    <img src=isbra_program_p2>
+    <img src=isbra_program_p3>
+    <img src=isbra_program_p4>
+    <img src=isbra_program_p5>
+    <img src=isbra_program_p6>
+    <img src=isbra_program_p7>
     </>
   );
 }
